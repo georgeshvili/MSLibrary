82f505411f68f3b69a1b7d98457ffe80b2ca4b27
George Djaniashvili <g.djaniashvili@gmail.com>
2019-08-07 18:59:01 +0300

    Changed realization of AssemblyBook.java

--- src/com.mslibrary/everythingForBook/AssemblyBook.java
+++ src/com.mslibrary/everythingForBook/AssemblyBook.java
@@ -1,15 +1,14 @@
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class AssemblyBook extends Book {
 
-    private LinkedList<String> worksNames;
-    private LinkedList<Integer> beginPages;
     HashMap<Integer, String> mapOfWorkNamesWithPages;
 
     public AssemblyBook(String booksName, String authorsLastName){
         super(booksName, authorsLastName);
-        this.mapOfWorkNamesWithPages = collectAssemblyBook();
+        this.mapOfWorkNamesWithPages = setWorksNamesAndBeginPages();
     }
 
     public AssemblyBook(String booksName, String authorsFirstName,
@@ -17,43 +16,18 @@ public class AssemblyBook extends Book {
         super(booksName, authorsFirstName, authorsLastName, genre, pages);
     }
 
-    public HashMap<Integer, String> getMapOfWorkNamesWithPages() {
+    public HashMap<Integer, String> setWorksNamesAndBeginPages(){
+        mapOfWorkNamesWithPages = new HashMap<Integer, String>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество работ: ");
+        int numberOfWorks = scanner.nextInt();
+        System.out.println("Введите номер страницы и название работы:");
+        for(int i = 0; i < numberOfWorks; i++)
+            mapOfWorkNamesWithPages.put(scanner.nextInt(), scanner.nextLine());
         return mapOfWorkNamesWithPages;
     }
 
-    private LinkedList<Integer> collectBeginPages(){
-
-        beginPages = new LinkedList<Integer>();
-
-        for(int i = 0; i <= 100; i+= 10){
-            beginPages.add(i);
-        }
-
-        return beginPages;
-
-    }
-    private LinkedList<String> collectWorksNames(){
-
-        worksNames = new LinkedList<String>();
-
-        for(int i = 0; i <= 100; i+=10){
-            worksNames.add("Произведение на странице номер -" + i);
-        }
-
-        return worksNames;
-
-    }
-
-    private HashMap<Integer, String> collectAssemblyBook(){
-
-        mapOfWorkNamesWithPages = new HashMap<Integer, String>();
-
-        this.beginPages = collectBeginPages();
-        this.worksNames = collectWorksNames();
-
-        for(int i = 0; beginPages.isEmpty() == false && worksNames.isEmpty() == false;)
-            mapOfWorkNamesWithPages.put(beginPages.remove(i), worksNames.remove(i));
-
+    public HashMap<Integer, String> getMapOfWorkNamesWithPages() {
         return mapOfWorkNamesWithPages;
     }
 
